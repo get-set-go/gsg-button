8d60761e7e9546df863c6833da1e41b1695f5dfc
AshwinH <ashwinh@cybage.com>
2016-12-13 17:59:58 +0530

    Add enable/disabled on button

--- examples/index.js
+++ examples/index.js
@@ -7,12 +7,20 @@ import Button from '../dist/index.min';
 
 ReactDOM.render(
   <div class="button-eg-container">
+
     <p class="button-eg-group">
       <h2>Button Type</h2>
 
       <Button bType="button">Button</Button>
       <Button bType="submit">Submit</Button>
       <Button bType="reset">Reset</Button>
+    </p>
+
+    <p class="button-eg-group">
+      <h2>Button State</h2>
+
+      <Button>Enabled Button</Button>
+      <Button isDisabled="true">Disabled Button</Button>
     </p>
 
   </div>,
